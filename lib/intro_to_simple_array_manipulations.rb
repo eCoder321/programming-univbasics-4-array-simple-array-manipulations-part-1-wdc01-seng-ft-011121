93b744dd7541f4be6eb3b75c5b08438d3b274c42
Fem Adisa <femiwonderful@gmail.com>
2020-11-05 23:50:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,6 +11,16 @@ def using_pop(array)
   array.pop 
 end
 
-def using_pop_with_args(array)
+def pop_with_args(array)
   array.pop(2)
 end
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def 
